9d97cf64726555677a057e963ea271e50d42baa9
elona2011 <arthuxx@gmail.com>
2020-06-18 20:41:28 +0800

    update

--- src/components/PublishPay.vue
+++ src/components/PublishPay.vue
@@ -111,14 +111,11 @@ export default {
                 signType, //微信签名方式：
                 paySign //微信签名
               },
-              function(res) {
+              res => {
                 console.log(res);
                 if (res.err_msg == "get_brand_wcpay_request:ok") {
                   console.log("success");
                   alert("充值成功");
-
-                  // 使用以上方式判断前端返回,微信团队郑重提示：
-                  //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
                 } else {
                   alert("充值失败");
                 }
