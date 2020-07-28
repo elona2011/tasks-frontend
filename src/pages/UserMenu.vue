26176f6f48781032286dce939f9f873368fa47de
elona2011 <arthuxx@gmail.com>
2020-07-28 22:21:19 +0800

    update

--- src/pages/UserMenu.vue
+++ src/pages/UserMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="height:100vh;">
     <div class="phone-viewport">
-      <user-title :title="title"  buttonName="我要发布任务" :isShow="false" />
+      <user-title :title="title"  buttonName="我要发布任务" :isShow="true" />
       <router-view></router-view>
       <md-bottom-bar md-sync-route class="bottom-bar">
         <md-bottom-bar-item
