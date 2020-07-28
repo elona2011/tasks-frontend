e5d5e51c5f681360f22520aed3f7f72d477b50b5
elona2011 <arthuxx@gmail.com>
2020-07-28 22:26:03 +0800

    update

--- src/pages/PublishMenu.vue
+++ src/pages/PublishMenu.vue
@@ -1,7 +1,7 @@
 <template>
   <div style="height:100vh;">
     <div class="phone-viewport">
-      <user-title :title="title" buttonName="我要做任务" :isShow="false" />
+      <user-title :title="title" buttonName="我要做任务" :isShow="true" />
       <keep-alive>
         <router-view></router-view>
       </keep-alive>
